d80b2d9179a84f91c770e6bcc11cfc1becf3fff4
Harishkumaresan <harishkumaresan14@gmail.com>
2024-02-19 16:18:33 +0530

    Commit

--- my-react-flow-app/src/App.jsx
+++ my-react-flow-app/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css'; // Assuming you have a CSS file for styling App component
 import Canvas from './components/Canvas';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap';
 
 function App() {
   return (
